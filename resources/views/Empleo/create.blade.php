3a55348fc32ae6f052792edab85eb5f6597efac1
joeAguirre <joseguir95@gmail.com>
2021-07-14 20:13:02 -0300

    primera modificacion del proyecto

---
+++ resources/views/Empleo/create.blade.php
@@ -0,0 +1,6 @@
+<h1>Este es el titulo</h1>
+
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolorum praesentium eveniet tenetur accusantium aut aperiam cumque harum cupiditate ex cum rem, doloremque,
+     consequuntur iure officia! Ipsum placeat rem provident?
+</p>
