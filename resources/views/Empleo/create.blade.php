f0e3db30acf683460adc7e53fdc8994b2a9e1336
joeAguirre <joseguir95@gmail.com>
2021-07-16 16:59:40 -0300

    agregue archivos create y edit en la carpeta empleo

--- resources/views/Empleo/create.blade.php
+++ resources/views/Empleo/create.blade.php
@@ -4,3 +4,7 @@
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolorum praesentium eveniet tenetur accusantium aut aperiam cumque harum cupiditate ex cum rem, doloremque,
      consequuntur iure officia! Ipsum placeat rem provident?
 </p>
+<p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis provident, ab hic quaerat tenetur asperiores voluptatum ex? Facere, saepe amet? Expedita
+     molestiae explicabo id suscipit, dolor recusandae aspernatur quas itaque?
+</p>
---
+++ resources/views/Empleo/edit.blade.php
@@ -0,0 +1,1 @@
+<h1>Aca va el formulario de editar</h1>
